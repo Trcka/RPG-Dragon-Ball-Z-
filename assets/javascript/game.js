4e2adc6d5a15b31b894ac7fb0610673220609faa
trcka <michael.trcka@ttu.edu>
2017-06-26 20:08:05 -0500

    starting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,45 @@
-var zFighters = {
-  goku : {
+
+ var goku = {
     healthPoints:700,
     attackPoints:150,
-    counter:175,
-  },
-  vegeta : {
+    counter:175
+  }
+var vegeta = {
     healthPoints:500,
     attackPoints:200,
     counter:200
-  },
-  gohan : {
+  }
+ var gohan = {
     healthPoints:1000,
     attackPoints:100,
-    counter:150,
-  },
-  yamcha : {
+    counter:150
+  }
+var  yamcha = {
     healthPoints:300,
     attackPoints:10,
     counter:10
   }
-};
-console.log(zfighters.goku.counter)
+console.log(goku.counter);
+
 //ending of Z-fighters var
-$("#hp").text(zfighters.goku.healthPoints);
+
+//pick a char, move char
+var fighterPicked = false;
+$(".btn").on("click", function(){
+  if(fighterPicked===false){
+  $(".hp").text(this.value);
+  console.log((this.value).healthPoints)
+
+}
+});
+
+
+
+
+
+
+
+
+
+
+
