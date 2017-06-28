a6b5c205f8a94132eab315cfe3122ea04a46d919
trcka <michael.trcka@ttu.edu>
2017-06-27 19:38:17 -0500

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,78 +1,117 @@
 $(document).ready(function() {
 
-var fighter ={
-  goku : {
-    healthPoints:700,
-    attackPoints:150,
-    counter:175
-  },
- vegeta : {
-    healthPoints:500,
-    attackPoints:200,
-    counter:200
-  },
- gohan : {
-    healthPoints:1000,
-    attackPoints:100,
-    counter:150
-  },  yamcha : {
-    healthPoints:300,
-    attackPoints:10,
-    counter:10
+  var fighter = {
+    goku: {
+      healthPoints: 7000,
+      attackPoints: 150,
+      counter: 175
+    },
+    vegeta: {
+      healthPoints: 5000,
+      attackPoints: 200,
+      counter: 200
+    },
+    gohan: {
+      healthPoints: 10000,
+      attackPoints: 100,
+      counter: 150
+    },
+    krillin: {
+      healthPoints: 3000,
+      attackPoints: 10,
+      counter: 10
+    }
   }
-}
+
+
+
   var yourHealth;
   var opponentHealth;
-//fighter.goku, 
-
-//ending of Z-fighters var
-
-//pick a char, move char
-var lock = false;
-function pickFighter() {
-  if(lock===false){
-  	var userHero = $(event.target);
-  	var sourceHero = userHero.src;
-    var heroID = userHero[0].id;
-    var hero = fighter[heroID];
-    userHero.hide();
-    $(".name").text(heroID);
-    $(".usersFighter").attr("src", sourceHero);
-    console.log(heroID);
-    lock=true;
-    console.log($(event.log))
-    console.log(event.log)
-    
-
-  };
-};
-
-function pickOpponent() {
-  if(lock===true){
-    var opponent = $(event.target);
-    var source = opponent.src;
-    var opponentID = opponent[0].id;
-    var opponentHero = fighter[opponentID];
-    opponent.hide();
-    //console.log(event.target);
-    $(".opName").text(opponentID);
-    $(".opponent").attr("src", source);
-    canFight=true;
-    console.log(opponentID);
-  };
-};
-var canFight= false;
-$("#fight").on("click",function(){
-  // if(canFight===true){
-  // 	var fighter=fighter[this.id];
-  	
-
-  	});
-
-$(".btn").on("click",function(){
-pickFighter();
-pickOpponent();
+  var yourAttack;
+  var opponentCounter;
+  var attackIncrease;
+  var opponentsDeafeated = 0;
 
 
-});
+
+  var isUserLocked = false;
+  var needToSetOpponent = true;
+
+
+  $(".fighter").click(function(event) {
+    if ((isUserLocked === false) && (needToSetOpponent === true)) {
+      var chosen = event.currentTarget;
+      var userFighter = fighter[chosen.id];
+      var source = $(event.target).attr("src");
+      $(event.currentTarget).hide()
+      $(".usersFighter").attr("src", source)
+      console.log(userFighter);
+      isUserLocked = true;
+      needToSetOpponent = true;
+      yourHealth = userFighter.healthPoints;
+      yourAttack = userFighter.attackPoints;
+      attackIncrease = userFighter.attackPoints;
+      $(".whatToDo").text("PICK A NEW OPPONENT!")
+    } else if (needToSetOpponent === true) {
+      var opponent = event.currentTarget;
+      var opponentFighter = fighter[opponent.id];
+      var source = $(event.target).attr("src");
+      $(event.currentTarget).hide()
+      $(".opponent").attr("src", source)
+      $("#opponentFighterDiv").append($(opponent).detach());
+      needToSetOpponent = false;
+      opponentHealth = opponentFighter.healthPoints;
+      opponentCounter = opponentFighter.counter;
+      opponentsDeafeated++
+
+
+
+    }
+  })
+
+  $("#fight").on("click", function() {
+
+
+
+    if (yourHealth <= 0) {
+      alert("you lose!");
+      $(".health").text(yourHealth);
+      $(".opHealth").text(opponentHealth);
+
+    } else if (opponentsDeafeated === 3) {
+
+      alert("you win")
+        //reset stuff
+        $(".health").text(yourHealth);
+        $(".opHealth").text(opponentHealth);
+    } else if(opponentHealth<=0) {
+    	$(".health").text(yourHealth);
+    	$(".opHealth").text(opponentHealth);
+    	needToSetOpponent=true;
+    	$(".whatToDo").text("PICK A NEW OPPONENT!")
+
+
+
+    }else{
+    	$(".whatToDo").text("FIGHT!!!")
+    	$(".health").text(yourHealth);
+    	$(".opHealth").text(opponentHealth);
+    	    opponentHealth -= yourAttack;
+    		yourHealth -= opponentCounter;
+    		yourAttack += attackIncrease;
+
+    }
+
+
+
+
+    console.log(yourHealth)
+    console.log(yourAttack)
+    console.log(opponentHealth)
+    console.log(opponentCounter)
+
+  })
+
+
+
 });
